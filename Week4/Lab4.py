0bfab137f280498fbe668e2c61275553f3d01cbc
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-09-28 14:21:39 -0400

    lab 4

--- Week4/Lab4.py
+++ Week4/Lab4.py
@@ -14,19 +14,28 @@ elif number < 0:
 else:   
         print("That number is zero")
 # 2. Write some code that asks the user for a number and prints out whether it is even or odd.
-number_two = (float(input("Hello, please enter another whole number: ")))
-
-if round(number_two) %  2 == 0:
+number_two = (float(input("Hello, please enter another whole number \n (note: the number will be rounded to the nearest integer regardless): ")))
+number_two_rounded = round(number_two, 0)
+if number_two_rounded %  2 == 0:
         print("That number is even when rounded to the nearest number")   
 else:   
         print("That number is oddwhen rounded to the nearest number")
 
-## it regesters it as odd if its a decimal number, cant round it... need to figure out how to do that... why not just put both answers....?
-print
+## problem going forward :it regesters it as odd if its a decimal number so it needs to be rounded to
+#    a whole number.... or an absolute vaue of the decimal number
+#       Fill out later,  feel like doing something else right now
 # 3. Write some code that asks the user for a number and prints out all the numbers from 1 to that number using a while loop.
+number_three = (float(input("Hello, please enter a positive whole number \n (note: I recommend one between 1 and 50): ")))
+while number_three > 0:
+        print(number_three)
+        number_three = number_three =+ 1
+         if number_three == 0:
+                print("done")
+                
+
 
 # 4. Use a for loop to ask a user for 5 numbers and print out the average of those 5 numbers.
-
+a
 # 5. Write some code that prints out all the numbers from 1 to 10 that are divisible by 3 or 5.
 
 # 6. Write some code that asks the user for a number and then prints out a countdown from that number to 1 using a while loop. After the countdown, print "Blast off!".
