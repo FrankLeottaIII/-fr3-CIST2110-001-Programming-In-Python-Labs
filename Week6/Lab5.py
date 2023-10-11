1c23b252e13c30a973ed159823792f6bd1c1a9a1
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-10-10 20:25:28 -0400

    my attempt at lab 5, -m flag not working

--- Week6/Lab5.py
+++ Week6/Lab5.py
@@ -1,25 +1,25 @@
 # Lab4
-# Author: 
+# Author: Frank R. Leotta III
 
 """_summary_
 This lab is designed to get you familiar with Python virtual environments as well as import statments to use external libraries.
 """
 
 # 1. Create a virtual environment called "venv" in the current directory. (Type command here in comments)
-
+python -m venv venv
 # 2. Activate the virtual environment. (Type command here in comments)
-
-# 3. Install the requests library. (Type command here in comments)
-
+source venv/bin/activate
+# 3. Install the requests library. (Type command here in comments) 
+install requests
 # 4. import requests library here
-
+import requests
 # 5. Use the requests library to make a GET request to https://api.github.com/events
-
+get = requests.get("https://api.github.com/events")
 # 6. Print out the status code of the response.
-
+print(get.status_code)
 # 7. Print out the content of the response.
-
+print(get.content)
 # 8. Print out the headers of the response.
-
+print(get.headers)
 # 9. Deactivate the virtual environment. (Type command here in comments)
-
+deactivate
