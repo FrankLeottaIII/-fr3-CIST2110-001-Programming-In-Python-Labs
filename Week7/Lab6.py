02ebc77ed0f2134908b40af0ef35539761c8781d
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-10-19 18:03:59 -0400

    started lab, will do it later

--- Week7/Lab6.py
+++ Week7/Lab6.py
@@ -1,8 +1,48 @@
 # Lab6
-# Author:
+# Author: Frank R. Leotta
 
 
 ## add in functions from test.py's test statements here to make the pytest work
+###Funtion 1
+def calculate_rectangle_area(length: float, width:float) -> float:
+    """
+    Calculates the area of a rectangle given its length and width.
+
+    Parameters:
+    length (float): The length of the rectangle.
+    width (float): The width of the rectangle.
+
+    Returns:
+    float: The area of the rectangle.
+    """
+    return length * width
+
+    
+#function 2    
+def calculate_hypotenuse():
+
+#function 3
+def is_even():
+
+
+#function 4
+def find_maximum():
+
+
+#function 5
+def grade_calculator():
+
+
+
+
+
+
+
+
+
+
+
+
 
 def main():
     pass
