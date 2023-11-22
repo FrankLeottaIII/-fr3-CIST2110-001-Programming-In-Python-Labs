07e5b2603f00cdd12e56ee05f9ace82e017e0574
Frank R. Leotta III <leottaif@go.stockton.edu>
2023-11-21 19:13:33 -0500

    lab 10 worked on

--- Week12/Lab10.py
+++ Week12/Lab10.py
@@ -8,27 +8,29 @@
 # Key: "age", Value: 30
 # Key: "city", Value: "Trenton"
 # Key: "state", Value: "New Jersey"
-
+my_dict = {"name": "John", "age": 30, "city": "Trenton", "state": "New Jersey"}
 
 # 2. Print out the dictionary.
-
+print(my_dict)
 
 # 3. Print out the value for the key "name".
-
+for key, value in my_dict.items():
+        if key == "name":
+            print(value)
 # 4. Lookup the key associated with the value "New Jersey" and print it out.
 # Hint 1: You will need to loop through the dictionary.
 # Hint 2: remember you can use the .items() method to get the key and value.
 
 # 5. Add a new key value pair to the dictionary.
 # Key: "country", Value: "USA"
-
+my_dict{"country"} = "USA"
 
 # 6. Print out the dictionary.
-
+print(my_dict)
 # 7. Remove the key value pair with the key "city".
-
+del my_dict["city"]
 # 8. Print out the dictionary.
-
+print(my_dict)
 # 9. Create a dictionary called cities with an key as the City name and values as a list that contains the state, population, and country.
 # use the following data:
 # Trenton, New Jersey, 84,913, USA
@@ -40,19 +42,54 @@
 # Philadelphia, Pennsylvania, 1,584,138, USA
 # San Antonio, Texas, 1,547,253, USA
 # San Diego, California, 1,423,851, USA
+# dictionary of dictionaries
 
-# 10. Print a table of the data using the pandas library.
+cities = {
+    "Trenton": {"state": "New Jersey", "Population": 84913, "Country": "USA"},# key and the value is another dicionary.   dictinary inside a dictionary
+    "New York City": {"state": "New York", "Population": 8336817, "Country": "USA"},
+    "Los Angeles": {"state": "California", "Population": 3979576, "Country": "USA"},
+    "Chicago": {"state": "Illinois", "Population": 2693976, "Country": "USA"},
+    "Houston": {"state": "Texas", "Population": 2320268, "Country": "USA"},
+    "Phoenix": {"state": "Arizona", "Population": 1680992, "Country": "USA"},
+    "Philadelphia": {"state": "Pennsylvania", "Population": 1584138, "Country": "USA"},
+    "San Antonio": {"state": "Texas", "Population": 1547253, "Country": "USA"},
+    "San Diego": {"state": "California", "Population": 1423851, "Country": "USA"}}
+
+# print(cities) #will get confusing.   being able to search a key and get a value is important
+# pandas is a good libary that will helpu us
+
+
+
+# 10. Print a table of the data using the pandas library.   #used for transforming data, lists dicionarys.  bunch of data tro trnsform and data midles   pands is the go to for that type of wokrk.  simple one
 # pip install pandas
-
+import pandas as pd
+#create a data frame, a word that holds you data
+# will know how to handle dicinoary of dicionaries
+df = pd.DataFrame(cities)
+print(df)  #  it will look like a table, much different and more organized in a easier to read fasion, common to do ... at the dope, a symbol you want to transpose data(reverse rows and columbs)
 
 # 11. Use the tabulate library to print out the table.
 # pip install tabulate
-
+from tabulate import tabulate # have the tabulate class here
+#try to pass it through tabulate
+""" impsort that tabluate muduple"""
+pretty_table = tabulate(df, headers="keys,..........") #pass in the dictionary and the headers
+#  sort of likes a sql table
+#tabulate olnly takes in lists.   if you put a dict to it, it will get erros
+#you need to convert the dict to a list if you want to use tabulate
+print("------------------")
+print(pretty_table)
+print("------------------")
+#pansas will covert your list
+#both are valid, and wil work on project 2.  You can do pandas,  unpack method as well for project 2
 
 # 11. Print out the population for the city of Chicago.
+print (cities["Chicago"].get("Population") #reference ky , its chacago(city names, the values are the diconary.
+       #inside the second ditionary, you can use.get()  to get value of population
+#last value you want to get, use .get()  to get the value of the key
+""" pandaz is slow, so might not be for ehtrythin g, """
 
 
-# 11. **Extra** An alternative to converting a dictionary into a pandas df then tabulate, you can use dictionary unpacking and tabulate.
 # Dictionary unpacking is a new feature in Python 3.9 and allows you to unpack a dictionary into a list of dictionaries.
 # You can then use tabulate to print out the table.
 
@@ -62,11 +99,41 @@
 # The ** operator unpacks the info dictionary into the new dictionary
 
 # Transform the data
+data = [{"City": city, **info} for city, info in cities.items()] #** going to deal with a dicionary within a dictionary... python knows
+""" cleaner to do it in one line, but its compressed
+it will end up returning a list of ditonarys.[ strubg comprension """
+#city ,info = key, value  For loops you can name what you want
+''' you can make it multible lines if you want it to'''
 
-# Print the table using tabulate
 
-
+#Print the table using tabulate
+#print(data)
+print(tabulate(data, headers="keys")) #keys are the cities
+'''a simple dable there
+if you wanted to d sql pass it thorugha  paramiter
+I am going to be using pandas alot, so it works for me.'''
 # 12. How many cities have a population greater than 1 million? Use a for loop to iterate through the dictionary.
+
+# counter valerible
+counter = 0
+for city, info in cities:  # for each line in the cities dictionary
+    if info["Population"] > 1000000: # city is each of the lines, we are [1] 2 3...   grabing each  population of each cities 
+        counter += 1 #if cities population is greater than 1 million, add 1 to the counter
+
+print(f'citeis with poulation grater than 1 million: {counter}')
+
+'''its a trial and error sort of think, it takes time to get used to it
+ the trick of it is getting all the data at once'''
 
 # 13.  How many cities are in the USA? Use .items() to get a list of tuples. Use a for loop to iterate through the list of tuples.
 # Hint 1: You will need to use the second item in the tuple. The second item is a dictionary. IE. for city, info in cities.items():
+
+#this is compared to the key valeu, another way to traverse a dicionary, , is the first way, this is the sencond way
+counter = 0
+cities.items() #this is a list of tuples
+#key is trention cvalue is all that stuf
+for city, info in cities.items(): #for each line in the cities dictionary
+    if info["Country"] == "USA": #if the country is USA (want to see if the country is USA)
+        counter += 1 #add 1 to the counter
+#there are 9 cities in the USA
+"""need to go through it again, missed the first part due to synch issues"""
